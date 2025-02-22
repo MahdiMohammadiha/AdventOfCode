64e6819b71ec1b22100d9c64fed8dddcdb3b0c60
Mahdi-Mohamadiha <mahdi.mohamadiha.znu@gmail.com>
2025-02-22 20:38:16 +0330

    First half and second half scripts for y2023 d1

---
+++ 2023/Day 1/first half.py
@@ -0,0 +1,46 @@
+"""
+    Note:
+        You may need to modify the configuration section
+        to match your file paths, inputs, or environment.
+"""
+
+# configurations
+FILE_NAME = "input.txt"
+
+# read file
+raw_datalist = open(FILE_NAME, "r")
+
+# convert each line to list item
+datalist = []
+for raw_data in raw_datalist.readlines():
+    datalist.append(raw_data.replace("\n", ""))
+
+
+# backwars the given word
+def reverse_word(arg):
+    return arg[::-1]
+
+
+# returns the first number in the given word
+def get_first_number(arg):
+    for letter in arg:
+        if letter.isnumeric():
+            return letter
+    raise Exception("No number found!")
+
+
+sum = 0
+for data in datalist:
+    # get first number
+    first_number = get_first_number(data)
+
+    # get last number
+    data = reverse_word(data)
+    last_number = get_first_number(data)
+
+    # pair the numbers
+    number = first_number + last_number
+
+    sum += int(number)
+
+print(sum)
